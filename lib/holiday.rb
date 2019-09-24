05451ad9d9dbfe91e6d6105f701fc175140691c2
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-24 16:11:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,8 +74,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holiday|
-    holiday.each do |
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies == "BBQ"
+        return holiday
+      end
+    end
+  end
 end
 
 
